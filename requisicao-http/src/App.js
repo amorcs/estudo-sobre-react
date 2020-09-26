02c754f9331a5ca48268586abd1a8c1eca65d790
Marcos Sousa <marco.sds@hotmail.com>
2020-09-26 17:41:59 -0300

    foi  requisicao http para a url em questão

--- requisicao-http/src/App.js
+++ requisicao-http/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+//https://sujeitoprogramador.com/rn-api/?api=posts
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+          responseHTTP : []
+        }
+    }
+    componentDidMount(){
+      const url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+
+      fetch(url)
+            .then((res)=>res.json())
+                .then((res)=>{
+                  this.setState({responseHTTP:res});
+                    
+                })
+    }
+    render(){
+      return(
+        <div>
+            {this.state.responseHTTP.map((item)=>{
+              return(
+                <h4>{item.titulo}</h4>
+              )
+            })}
+        </div>
+      );
+    }
 }
-
-export default App;
