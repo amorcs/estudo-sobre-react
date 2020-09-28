2c45192d2e6308674ccc7488297043f3ebd0e6b0
Marcos Sousa <marco.sds@hotmail.com>
2020-09-28 12:12:31 -0300

    ajustes na funcionalidade de requisicao HTTP e inicio da estilização CSS

--- requisicao-http-rotas/src/pages/home/index.js
+++ requisicao-http-rotas/src/pages/home/index.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
+import { HashLink } from 'react-router-hash-link';
 import './home.css';
-export default class Home extends Component{
+//https://sujeitoprogramador.com/r-api/?api=filmes
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            filmes: [],
+        }
+    }
+    componentDidMount(){
+        let url = 'https://sujeitoprogramador.com/r-api/?api=filmes';
+
+        fetch(url).
+            then((response)=> response.json()).
+                then((response)=>{
+                    this.setState({filmes:response})
+                })
+
+    }
     render() {
         return (
             <div className="containerHome">
-                <h1>Conteúdo da Home</h1>
+               
+                {this.state.filmes.map((filme)=>{
+                    return(
+                        <div className="containerHomeFilho">
+                            <article key={filme.id}>
+                                <strong>{filme.nome}</strong>
+                                <img src={filme.foto}/>
+                                <HashLink to="/">Acessar</HashLink>
+                            </article>
+                            
+                        </div>
+                    )                    
+                })}
             </div>
         );
     }
 }
+export default Home;
