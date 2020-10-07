a6866b890a5e76a4819b3dd7d66cff7f9557a037
Marcos Sousa <marco.sds@hotmail.com>
2020-10-07 16:36:34 -0300

    criacao de tela de cadastro e login de usuario com o Firebase

--- proj-clone/src/App.js
+++ proj-clone/src/App.js
@@ -13,51 +13,66 @@ class App extends Component {
     this.sair = this.sair.bind(this);
   }
   componentDidMount() {
-
-
+    firebase.auth().onIdTokenChanged((user) => {
+      this.setState({ user: user })
+    })
   }
   cadastrar(e) {
     firebase.auth()
-          .createUserWithEmailAndPassword(this.state.email, this.state.senha)
-                .then(()=>{
-                    this.setState({email:'', senha:''});
-                    alert('Usuário cadastrado com sucesso!')
-                })
-                .catch((error)=>{
-                    alert(error.code)
-                });
+      .createUserWithEmailAndPassword(this.state.email, this.state.senha)
+      .then(() => {
+        this.setState({ email: '', senha: '' });
+        alert('Usuário cadastrado com sucesso!')
+      })
+      .catch((error) => {
+        alert(error.code)
+      });
     e.preventDefault();
   }
   entrar() {
-
+    firebase.auth()
+      .signInWithEmailAndPassword(this.state.email, this.state.senha)
+      .then(() => {
+        alert('Usuário logado com Sucesso!');
+        this.setState({ email: '', senha: '' })
+      })
+      .catch((error) => {
+        alert(error.code)
+      });
   }
   sair() {
-
+    firebase.auth().signOut();
+    alert('usuário deslogado!')
   }
 
   render() {
+
     return (
       <div>
-        
-        <div>
-          <h3>Senha BemVindo!</h3>
-          <h4>Usuário: </h4>
-          <button onClick={this.sair}>sair</button>
-        </div>
-        <div>
-          <label>Email:</label>
-          <br />
-          <input value={this.state.email}
-            onChange={(e) => this.setState({ email: e.target.value })} />
-          <br />
-          <label>Senha:</label>
-          <br />
-          <input value={this.state.senha}
-            onChange={(e) => this.setState({ senha: e.target.value })} />
-          <br />
-          <button onClick={this.cadastrar}>cadastrar</button>
-          <button onCLick={this.entrar}>entrar</button>
-        </div>
+        {this.state.user ?
+          <div>
+            <h3>Senha Bem-Vindo!</h3>
+            <h4>Usuário: { this.state.user.email } </h4>
+            <h4>Usuário: { this.state.user.uid } </h4>
+            <button onClick={this.sair}>sair</button>
+          </div>
+          :
+          <div>
+            <h3>Formulário de Login:</h3>
+            <label>Email:</label>
+            <br />
+            <input value={this.state.email}
+              onChange={(e) => this.setState({ email: e.target.value })} />
+            <br />
+            <label>Senha:</label>
+            <br />
+            <input value={this.state.senha}
+              onChange={(e) => this.setState({ senha: e.target.value })} />
+            <br />
+            <button onClick={this.cadastrar}>cadastrar</button>
+            <button onClick={this.entrar}>entrar</button>
+          </div>
+        }
       </div>
     );
   }
